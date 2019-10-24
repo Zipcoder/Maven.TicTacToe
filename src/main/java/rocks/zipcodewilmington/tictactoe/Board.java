1e7a5ef20f5b914a52c0acf4c3e28467ab326959
michael-doll <MWDoll88@gmail.com>
2019-10-24 17:17:03 -0400

    clean comments

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,18 +4,15 @@ import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
 
 /**
  * @author leon on 6/22/18.
+ * Michael Doll 10.24.2019
  */
 
 public class Board {
-
     private Character[][] board;
-
     public Board(Character[][] matrix) {
     this.board = matrix;
 
     }
-//Should be able to simply this and stop repeating*******************
-    //***************************************************************
     public  boolean checkHorizontal(Character checker) {
         for (int i = 0; i <= 2; i++) {
             if (board[i][0] == checker && board[i][1] == checker && board[i][2] == checker ){
@@ -56,32 +53,7 @@ public class Board {
     public Boolean isTie() { return (getWinner().equals("")); }
 
     public String getWinner() {
-//        String winner = "";
-//        for( int i = 0; i <=2; i++) {
-//            if (board[i][0] ==  board[i][1] && board[i][0] == board[i][2]) {
-//                if(board[i][i] == 'X'){
-//                    winner = "X";
-//                }else if (board[i][i] == 'O'){
-//                    winner = "O";
-//                }
-//            }
-//        }
-//        for( int i = 0; i <=2; i++) {
-//            if (board[0][i] ==  board[1][i] && board[0][i] == board[2][i]) {
-//                if(board[i][i] == 'X') { // pass in char and check
-//                    winner = "X";
-//                }else if (board[i][i] == 'O'){
-//                winner = "O";
-//            }
-//            }
-//        }
-////        if (isInFavorOfO()){
-////            winner = "O";
-////        }
-////        else if(isInFavorOfX()){
-////            winner = "X";
-////        }
-
+        
         if (checkDiag('X') || checkVertical('X') || checkHorizontal('X')) {
             return ("X");
         }
