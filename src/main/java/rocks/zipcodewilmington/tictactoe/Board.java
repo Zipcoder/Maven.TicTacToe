bc05a7aa33cf66ca00fb63951dd70161eb8ec63c
Mike King <michkin32@gmail.com>
2019-10-24 15:08:28 -0400

    Made some progress

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,62 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] board;
+
     public Board(Character[][] matrix) {
     }
+    public Boolean checkHorizontal(Character hT){
+        for (int i = 0; i <= 2; i++){
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+                return true;
+            }
+        }
+        return false;
+    }
+    public Boolean checkVertical(Character hT){
+        for (int i = 0; i <= 2; i++){
+            if ((board[0][i] == hT && board[1][i] == hT && board[2][i] == hT) ||
+                    (board[2][0] == hT && board[1][1] == hT && board[0][2] == hT));
+            return true;
+        }
+        return false;
+    }
+    public Boolean checkDiagonal(Character hT){
+        if ((board[0][0] == hT && board[1][1] == hT && board[2][2] == hT) ||
+        (board[2][0] == hT && board[1][1] == hT && board[0][2] == hT)){
+                return true;
+            }
 
+        return false;
+    }
     public Boolean isInFavorOfX() {
-        return null;
+
+        if (checkHorizontal(('X')) || checkVertical('X') || checkVertical('X')){
+            return true;
+        }return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+
+        if (checkHorizontal(('O')) || checkVertical('O') || checkVertical('O')){
+            return true;}
+
+            return false;
     }
 
-    public Boolean isTie() {
-        return null;
+    public Boolean isTie(){
+    if (!isInFavorOfO() || !isInFavorOfX());
+        return true;
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfX()){
+           return "X";
+        }
+        if (isInFavorOfO()){
+            return "O";
+        }
+        return "";
     }
 
 }
