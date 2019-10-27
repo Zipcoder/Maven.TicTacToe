9cb02e1c31ab8a4ee7494fa942baf12a0f385048
mike <mike.schoenbeck@gmail.com>
2019-10-26 23:48:44 -0400

    methods and variables completed wrong way

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -9,40 +9,49 @@ public class Board {
     }
 
     private Character[][] board;
-    private boolean leftVerticalXWin = board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X';
-    private boolean centerVerticalXWin =board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X';
-    private boolean rightVerticalXWin = board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X';
 
-    private boolean topHorizontalXWin = board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X';
-    private boolean middleHorizontalXWin = board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X';
-    private boolean bottomHorizontalXWin = board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X';
 
-    private boolean leftVerticalYWin = board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X';
-    private boolean centerVerticalYWin;
-    private boolean rightVerticalYWin;
+    private boolean xLeftVerticalWin = board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X';
+    private boolean xCenterVerticalWin = board[0][0] == 'X' && board[1][1] == 'X' && board[2][1] == 'X';
+    private boolean xRightVerticalWin = board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X';
 
-    private boolean topHorizontalYWin;
-    private boolean middleHorizontalYWin;
-    private boolean bottomHorizontalYWin;
+    private boolean xTopHorizontalWin = board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X';
+    private boolean xMiddleHorizontalWin = board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X';
+    private boolean xBottomHorizontalWin = board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X';
 
-    private boolean diagonalDownToRightXWin = board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X';
-    private boolean diagonalUpToLeftXWin;
+    private boolean oLeftVerticalWin = board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O';
+    private boolean oCenterVerticalWin = board[0][0] == 'O' && board[1][1] == 'O' && board[2][1] == 'O';
+    private boolean oRightVerticalWin = board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O';
 
-    private boolean diagonalDownToRightYWin;
-    private boolean diagonalUpToLeftYWin;
+    private boolean oTopHorizontalWin = board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O';
+    private boolean oMiddleHorizontalWin = board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O';
+    private boolean oBottomHorizontalWin = board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O';
+
+    private boolean xDiagonalDownToRightWin = board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X';
+    private boolean xDiagonalUpToLeftWin = board[2][0] == 'X' && board[1][1] == 'X' && board[0][2] == 'X';
+
+    private boolean oDiagonalDownToRightWin = board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O';
+    private boolean oDiagonalUpToLeftWin = board[2][0] == 'O' && board[1][1] == 'O' && board[0][2] == 'O';
 
     public Boolean isInFavorOfX() {
-        return leftVerticalXWin || centerVerticalXWin || rightVerticalXWin || topHorizontalXWin || middleHorizontalXWin || bottomHorizontalXWin;
 
+        for(int row = 0; row <= 2; row++)   {
+            for(int column = 0; column <= 2; column++)  {
+
+            }
+        }
+
+        return xLeftVerticalWin || xCenterVerticalWin || xRightVerticalWin || xTopHorizontalWin ||
+                xMiddleHorizontalWin || xBottomHorizontalWin || xDiagonalDownToRightWin || xDiagonalUpToLeftWin;
     }
 
-
     public Boolean isInFavorOfO() {
-        return null;
+        return oLeftVerticalWin || oCenterVerticalWin || oRightVerticalWin || oTopHorizontalWin ||
+                oMiddleHorizontalWin || oBottomHorizontalWin || oDiagonalDownToRightWin || oDiagonalUpToLeftWin;
     }
 
     public Boolean isTie() {
-        return null;
+        return (!isInFavorOfX() && isInFavorOfO());
     }
 
     public String getWinner() {
