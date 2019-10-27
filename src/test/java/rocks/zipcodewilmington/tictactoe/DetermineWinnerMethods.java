4d503b3212fa467a2b6c9f69937fade3f0dad6f3
mike <mike.schoenbeck@gmail.com>
2019-10-27 14:25:33 -0400

    Completed lab

---
+++ src/test/java/rocks/zipcodewilmington/tictactoe/DetermineWinnerMethods.java
@@ -0,0 +1,11 @@
+package rocks.zipcodewilmington.tictactoe;
+
+/*classpublic class DetermineWinnerMethods {
+    Character X = 'X';
+    Character O = 'O';
+    public Character getHorizontalWinner()   {
+        for( int row = 0; row <= 2; row++)  {
+            if(this.)
+        }
+    }
+}*/
