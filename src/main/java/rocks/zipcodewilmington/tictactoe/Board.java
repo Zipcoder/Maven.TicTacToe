588b4240c3e1cca1cefe3e032391eee42b890724
sitarameez <sitarablossom@gmail.com>
2021-07-02 00:32:04 -0400

    initial commit player x or o

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,19 +4,65 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
+
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        int countOfX = 0;
+        int countOfO = 0;
+        Character[][] board = this.matrix;
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(board[i][j] == 'X'){
+                    countOfX++;
+                } else if(board[i][j] == 'O'){
+                    countOfX++;
+                }
+            }
+        }
+        return countOfX > countOfO;
     }
 
+
     public Boolean isInFavorOfO() {
-        return null;
+        int countOfX = 0;
+        int countOfO = 0;
+        Character[][] board = this.matrix;
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(board[i][j] == 'X'){
+                    countOfX++;
+                } else if(board[i][j] == 'O'){
+                    countOfX++;
+                }
+            }
+        }
+        return countOfO > countOfX;
     }
 
     public Boolean isTie() {
-        return null;
+        Character[][] board = this.matrix;
+        //Board board = new Board(new Character[3][3]);
+
+//       for(int i=0;i<3;i++) {
+//           for (int j = 0; j < 3; j++){
+//
+//               if (board[i][0]&&[i][1]&&[i][2])
+//
+//
+//           if ([0][j]&&[1][j]&&[2][j])
+//
+//
+//           if ([i][j]==[j][i]){
+//
+//       }
+        return true;
     }
 
     public String getWinner() {
