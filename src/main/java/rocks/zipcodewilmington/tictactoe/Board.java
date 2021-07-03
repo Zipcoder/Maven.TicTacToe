7d6a7b8b60ee7c0f92f8f47210b303a8e2fc2654
Dipinti <dipinti@zipcodes-MBP.fios-router.home>
2021-07-03 11:58:27 -0400

    Tic Tac Toe array of arrays

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,73 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    private Character[][] matrix;
+
     public Board(Character[][] matrix) {
-    }
-
-    public Boolean isInFavorOfX() {
-        return null;
-    }
-
-    public Boolean isInFavorOfO() {
-        return null;
-    }
-
-    public Boolean isTie() {
-        return null;
+        this.matrix = matrix;
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfO())
+        {
+            return "O";
+        }
+        else if (isInFavorOfX())
+        {
+            return "X";
+        }
+        else { return ""; }
     }
+
+    public Boolean isInFavorOfX() {
+        boolean result = false;
+        for (int i = 0; i < matrix.length; i++) {
+            //checks row wise
+            if (matrix[i][0] == ('X') && matrix[i][1] == ('X') && matrix[i][2] == ('X')) {
+                result = true;
+            }
+            //checks column wise
+            else if (matrix[0][i] == ('X') && matrix[1][i] == ('X') && matrix[2][i] == ('X')) {
+                result = true;
+            }
+            //checks diagonal wise
+            else if ((matrix[0][0] == ('X') && matrix[1][1] == ('X') && matrix[2][2] == ('X'))
+                    || (matrix[0][2] == ('X') && matrix[1][1] == ('X') && matrix[2][0] == ('X'))) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public Boolean isInFavorOfO() {
+        boolean result = false;
+        for (int i = 0; i < matrix.length; i++) {
+            //checks row wise
+            if (matrix[i][0] == ('O') && matrix[i][1] == ('O') && matrix[i][2] == ('O')) {
+                result = true;
+            }
+            //checks column wise
+            else if (matrix[0][i] == ('O') && matrix[1][i] == ('O') && matrix[2][i] == ('O')) {
+               result = true;
+            }
+            //checks diagonal wise
+            else if ((matrix[0][0] == ('O') && matrix[1][1] == ('O') && matrix[2][2] == ('O'))
+                    || (matrix[0][2] == ('O') && matrix[1][1] == ('O') && matrix[2][0] == ('O'))) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public Boolean isTie() {
+        if (getWinner().equals("X"))
+        { return false; }
+        else if (getWinner().equals("O"))
+        { return false; }
+        else { return true; }
+    }
+
+
 
 }
